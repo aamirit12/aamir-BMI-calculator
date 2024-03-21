a5bf33f2876cb6be941e1365b165288ad3f89a5d
aamirali001 <seoaamir1@gmail.com>
2024-03-21 12:32:59 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+import  inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {message: "Enter your Body weight in Kg", type: "number", name: "weightInKg"},
+{message: "Enter your height in meters", type: "number", name: "heightInKg"},
+])
+const bmi = answer.weightInKg / (answer.heightInKg * answer.heightInKg);
+console.log(`YOUR BMI IS ${bmi}`);
